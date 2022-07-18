304c4d26599b25651bddbbe5ef2d2d212f3334d0
lordmicrowave <89773980+lordmicrowave@users.noreply.github.com>
2022-07-18 19:30:00 +0200

    Create index.js

---
+++ src/Components/index.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar/Navbar";
+import Post from "./Post/Post";
+import Posts from "./Posts/Posts";
+import Postone from "./Post/Postone";
+import Posttwo from "./Post/Posttwo";
+import Postthree from "./Post/Postthree";
+import Postfour from "./Post/Postfour";
+import Postfive from "./Post/Postfive";
+import Stories from "./Stories/Stories";
+import Story from "./Story/Story";
+import Suggestions from "./Suggestions/Suggestions";
+import Conversation from "./Conversation/Conversation";
+
+
+export { Navbar, Post, Stories, Story, Posts, Suggestions, Conversation, Postone, Posttwo, Postthree, Postfour, Postfive };
