3e1a430c27aaa32ec60bb06c1ac7dd94c4d3486b
lordmicrowave <89773980+lordmicrowave@users.noreply.github.com>
2022-07-18 19:26:57 +0200

    Create index.js

---
+++ src/Pages/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import Explore from "./Explore/Explore";
+import Direct from "./Direct/Direct";
+import Notfound from "./Notfound/Notfound";
+import Card from "../Pages/Explore/Card";
+import Cardone from "../Pages/Explore/Cardone";
+import Cardtwo from "../Pages/Explore/Cardtwo";
+import Profile from "./Profile/Profile";
+import Saved from "./Saved/Saved";
+
+export { Home, Login, Register, Explore, Direct, Notfound, Profile, Saved, Card, Cardone, Cardtwo};
