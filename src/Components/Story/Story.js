dbd7a1e0a5fd6a37ed241f5ff627cb0a1fac58e3
lordmicrowave <89773980+lordmicrowave@users.noreply.github.com>
2022-07-18 19:30:36 +0200

    Add files via upload

---
+++ src/Components/Story/Story.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { profile } from "C:\Users\Sasha-Lee\OneDrive - University of Witwatersrand\Desktop\INTERACTIVE MEDIA\assignmenttwo\src\Icons";
+import "./Story.scss";
+
+const Story = () => {
+  return (
+    <div className="app__stories-container">
+      <img src={profile} alt="" />
+    </div>
+  );
+};
+
+export default Story;
